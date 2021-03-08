ea5dc4dfdbfc5cdf81d872ad366b198362892a60
PatoGuereque <patoguereque@gmail.com>
2021-03-08 14:08:34 -0600

    Add xpm and ypm functions

--- lib/distancias/distancias.h
+++ lib/distancias/distancias.h
@@ -5,5 +5,7 @@ float dlr(int, int, int, int);
 int dh(int, int, int, int);
 int dv(int, int, int, int);
 int abs(int);
+int xpm(int x1, int y1, int x2, int y2);
+int ypm(int x1, int y1, int x2, int y2);
 
 #endif
--- src/main.c
+++ src/main.c
@@ -3,7 +3,7 @@
 
 int main(int argc, char *argv[]) {
     float dlrecta;
-    int dhoriz, dvert;
+    int dhoriz, dvert, xpm_res, ypm_res;
             
     dlrecta = dlr(3, 4, 10, 12);
     printf("Distancia en línea recta: %f\n", dlrecta);
@@ -13,5 +13,10 @@ int main(int argc, char *argv[]) {
     printf("Distancia horizontal: %d\n", dhoriz);
     printf("Distancia vertical: %d\n", dvert);
 
+    printf("Punto A: (0, 0), Punto B: (10, 20)\n");
+    xpm_res = xpm(0, 0, 10, 20);
+    ypm_res = ypm(0, 0, 10, 20);
+    printf("Coordenada x en medio: %d\n", xpm_res);
+    printf("Coordenada y en medio: %d\n", ypm_res);
     return 0;
 }
